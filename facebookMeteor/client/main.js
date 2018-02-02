743ba4d0ac5e5cb57261220b909bcf67bc5e51fa
Rostyslav Gogolauri <go.go.gg.rostislav@gmail.com>
2018-02-02 20:19:38 +0200

    Add files via upload

--- facebookMeteor/client/main.js
+++ facebookMeteor/client/main.js
@@ -1,6 +1,6 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-
+import { Accounts } from 'meteor/accounts-base'
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
@@ -20,3 +20,26 @@ Template.hello.events({
     instance.counter.set(instance.counter.get() + 1);
   },
 });
+
+Meteor.loginWithMicrosoft({
+    requestOfflineToken: true,
+    requestPermissions: ['wl.emails'] // Permission scopes are found here: https://msdn.microsoft.com/en-us/library/hh243648.aspx
+}, function(error) {
+    if (error) {
+        console.error('Login failed:', error.reason || error);
+    }
+    else {
+        console.log('Logged in!');
+   		 }
+});
+ServiceConfiguration.configurations.upsert(
+  { service: 'microsoft' },
+  {
+
+    $set: {
+      loginStyle: "popup",
+      clientId: "b9a04d2a-b015-491b-860b-5794ee9e2d2a", // See table below for correct property name!
+      secret: "kdeivrGY009#}fQKTNT66{^"
+    }
+  }
+);
